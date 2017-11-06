5dfb290587d0120d58401f0d0100ac04c55803a2
Benjamin Ross <ross.benjamin.cem@gmail.com>
2017-11-05 19:04:07 -0500

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,46 @@
 
 #include "CCBot.h"
 
+class Replay : public sc2::ReplayObserver {
+public:
+    std::vector<uint32_t> count_units_built_;
+
+    Replay() :
+        sc2::ReplayObserver() {
+    }
+
+    void OnGameStart() final {
+        std::cout << "OnGameStart" << std::endl;
+        const sc2::ObservationInterface* obs = Observation();
+        assert(obs->GetUnitTypeData().size() > 0);
+        count_units_built_.resize(obs->GetUnitTypeData().size());
+        std::fill(count_units_built_.begin(), count_units_built_.end(), 0);
+    }
+
+    void OnUnitCreated(const sc2::Unit* unit) final {
+        std::cout << "Units created:" << std::endl;
+        assert(uint32_t(unit->unit_type) < count_units_built_.size());
+        ++count_units_built_[unit->unit_type];
+    }
+
+    void OnStep() final {
+    }
+
+    void OnGameEnd() final {
+        std::cout << "Units created:" << std::endl;
+        const sc2::ObservationInterface* obs = Observation();
+        const sc2::UnitTypes& unit_types = obs->GetUnitTypeData();
+        for (uint32_t i = 0; i < count_units_built_.size(); ++i) {
+            if (count_units_built_[i] == 0) {
+                continue;
+            }
+
+            std::cout << unit_types[i].name << ": " << std::to_string(count_units_built_[i]) << std::endl;
+        }
+        std::cout << "Finished" << std::endl;
+    }
+};
+
 
 int main(int argc, char* argv[]) 
 {
@@ -69,6 +109,16 @@ int main(int argc, char* argv[])
     //          The bot may crash or do unexpected things if its logic is not called every frame
     coordinator.SetStepSize(stepSize);
     coordinator.SetRealtime(false);
+
+    // folder exists
+    if (!coordinator.SetReplayPath("Replays/"))
+    {
+        std::cout << "Unable to find replays." << std::endl;
+        exit(-1);
+    }
+
+    Replay replay_observer;
+    coordinator.AddReplayObserver(&replay_observer);
 
     coordinator.SetParticipants({
         CreateParticipant(Util::GetRaceFromString(botRaceString), &bot),
